7c5d1531926b2f560d28eaf1ab60ab01e94fcfa1
Manrique Kraus <krausmanrique@gmail.com>
2024-01-06 14:07:25 -0300

    header finalizado e layout do mecanismo de busca iniciado

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,14 @@
+window.sr = ScrollReveal({ reset: true });
+
+ScrollReveal().reveal('.container-busca', {
+    origin: 'top',
+    distance: '40px',
+    duration: 2000,
+    delay: 200,
+    easing: 'cubic-bezier(0, 0, 0, 1)',
+});
+
+
 function aparecerMenu() {
     const menuMobile = document.querySelector('.menu-mobile');
     let botaoAbrir = document.querySelector(".icone-menu-mobile");
