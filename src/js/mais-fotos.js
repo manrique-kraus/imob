1b96bb036bb79aeb5276c7e0da287627a0c431ac
Manrique Kraus <krausmanrique@gmail.com>
2024-09-08 13:16:48 -0300

    imoveis parcialmente adicionados

--- src/js/mais-fotos.js
+++ src/js/mais-fotos.js
@@ -1,7 +1,7 @@
 //mais fotos
 
 document.getElementById("mais-fotos").addEventListener("click", function(event) {
-    event.preventDefault(); 
-    document.querySelector('.imagens-todas').classList.toggle('esconder');
-    document.querySelector('.imagens').classList.toggle('esconder');
-  });
+  event.preventDefault(); 
+  document.querySelector('.imagens-todas').classList.toggle('esconder');
+  document.querySelector('.imagens').classList.toggle('esconder');
+});
